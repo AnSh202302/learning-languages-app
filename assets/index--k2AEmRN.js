034acd98e6b89513d3bde6704707241fea33edb2
AnSh202302 <AnSh202302@users.noreply.github.com>
2024-02-16 22:36:09 +0000

    deploy: b31fa8e271e904302aa27c8da778aba2c4724ebe

---
+++ assets/index--k2AEmRN.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CpGX9xn_.js";const s=()=>e.jsx("h1",{children:"Teachers"});export{s as default};
---
+++ assets/index-DDowiCoT.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CpGX9xn_.js";const e=()=>r.jsx("h1",{children:"Favorites"});export{e as default};

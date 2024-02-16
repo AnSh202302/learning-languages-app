7a30c31ef3c52012992335ba77d24246af45703b
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-02-17 00:39:56 +0100

    add: 404

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/learnlingo-app">
+    <BrowserRouter basename="/learnlingo-app/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
